ec8b8760749049297ece4de8fc12a3ed771b5a49
hundodayso <solder-elision.0y@icloud.com>
2025-07-31 22:32:21 +0100

    day 59 progressing

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template, url_for
+import requests
+
+
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def get_all_posts():
-    return render_template("index.html")
+    blog_data_ep = "https://api.npoint.io/45c4981c26ffed6e4fad"
+    all_posts = requests.get(blog_data_ep).json()
+    return render_template("index.html", posts=all_posts)
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
